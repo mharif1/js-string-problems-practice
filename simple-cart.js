7ce997738def56ee343037a4e70c62bd2ad25b13
mharif1 <md5667676@gmail.com>
2022-08-08 20:06:48 +0600

    22

---
+++ simple-cart.js
@@ -0,0 +1,11 @@
+const shoppingcart = [
+    {name: 'shoe', price : 2600},
+    {name:'shirt', price : 1800},
+    {name: 'watch', price : 20000},
+    {name: 'pant',  price : 1500}   
+]
+function totalCost(products){
+    for(let i = 0; i < shoppingcart.length; i++){
+        
+    }
+}
